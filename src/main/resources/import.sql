11512193977ca1ce1fafe16ad8e1fb0dc96c398e
RobertMili <robertmilicevic3869@gmail.com>
2023-07-10 12:45:01 +0200

    check sql database, something is wrong with

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,3 @@
-CREATE TABLE fruit (
-                                        id serial PRIMARY KEY,
-                                        name varchar(255)
-    );
-
-
 INSERT INTO fruit(id, name) VALUES (1, 'Cherry');
 INSERT INTO fruit(id, name) VALUES (2, 'Apple');
 INSERT INTO fruit(id, name) VALUES (3, 'Banana');
