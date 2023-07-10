acace291816109869bd4a9a3022a181d20729bba
RobertMili <robertmilicevic3869@gmail.com>
2023-07-10 12:13:07 +0200

    adding import.sql / database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+CREATE TABLE fruit (
+                                        id serial PRIMARY KEY,
+                                        name varchar(255)
+    );
+
+
+INSERT INTO fruit(id, name) VALUES (1, 'Cherry');
+INSERT INTO fruit(id, name) VALUES (2, 'Apple');
+INSERT INTO fruit(id, name) VALUES (3, 'Banana');
+ALTER SEQUENCE fruit_seq RESTART WITH 4;
